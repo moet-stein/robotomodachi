09c2dc91e14b3f0e178f26ff0cf4327aa872d55c
moe-stein <0101.mste@gmail.com>
2020-07-15 21:21:39 +0900

    Updates

---
+++ precache-manifest.7fd579fbe740dcc5644a9183a5d90103.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9a190ce6ce72d2de09ae027a08eece",
+    "url": "/robotomodachi/index.html"
+  },
+  {
+    "revision": "90a80af2e0152cb0bdf8",
+    "url": "/robotomodachi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c6572b2415dab27b2e9c",
+    "url": "/robotomodachi/static/css/main.51411d9b.chunk.css"
+  },
+  {
+    "revision": "90a80af2e0152cb0bdf8",
+    "url": "/robotomodachi/static/js/2.cfcaf103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotomodachi/static/js/2.cfcaf103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6572b2415dab27b2e9c",
+    "url": "/robotomodachi/static/js/main.96e64962.chunk.js"
+  },
+  {
+    "revision": "dd501edfe0345e584595",
+    "url": "/robotomodachi/static/js/runtime-main.f82ba717.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotomodachi/static/media/SEGA.12a3db8b.woff"
+  }
+]);
